39ab7bd069f84f07f6861dcdc89e05e79842c28c
k-marinov <linxmap@gmail.com>
2020-01-10 20:27:25 +0000

    zap

---
+++ CategoryTheory.playground/Pages/Part1-The Essence of Composition.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/// ## Introduction
+/// A category is a suprisingly simple concept. A category consists of objects and arrows that go between
+/// them. But the essence of a category is composition. Or the essence of composition is a category.
+/// Arrows compose, if you have an arrow from object A to object B and another arrow from object B to object C,
+/// then there must be an arrow - their composition - that goes from A to C
+
+
+/// 1.1 Arrows as Functions
